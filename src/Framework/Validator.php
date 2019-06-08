e3e5650497b64dfdc0ba47b1725016ad89e98db2
Vasco Compain <vasco.gribouille@gmail.com>
2019-06-08 16:01:26 +0200

    Ajout du Validateur

---
+++ src/Framework/Validator.php
@@ -0,0 +1,183 @@
+<?php
+namespace Framework;
+
+use DateTime;
+use Framework\Validator\ValidationError;
+use mysql_xdevapi\Exception;
+
+class Validator {
+
+    /**
+     * @var array
+     */
+    private $fields;
+
+    /**
+     * @var string[]
+     */
+    private $errors = [];
+
+    public function __construct(array $fields)
+    {
+        $this->fields = $fields;
+    }
+
+    /**
+     * Permet de rendre requis des champs
+     * @param string ...$fields
+     * @return self
+     */
+    public function required(string ...$fields): self
+    {
+        foreach ($fields as $field) {
+            if (!array_key_exists($field, $this->fields)) {
+                $this->addError($field, 'required');
+            }
+        }
+        return $this;
+    }
+    
+    /**
+     * Versifie que les champs sont presents et non-vides
+     * @param string ...$fields
+     * @return self
+     */
+    public function filled(string ...$fields): self
+    {
+        foreach ($fields as $field) {
+            $value = $this->getValue($field);
+            if (is_null($value) OR empty(trim($value))) {
+                $this->addError($field, 'slug');
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * @param string $field
+     * @param int|null $min
+     * @param int|null $max
+     * @return Validator
+     * @throws \Exception
+     */
+    public function length(string $field, ?int $min, ?int $max = null): self
+    {
+        if (is_null($min) AND is_null($max)) {
+            throw new \Exception(
+                'Validation error: you need to define a least one length parameters in addition to the field name.',
+                E_ERROR
+            );
+        }
+        if (!is_null($min) AND !is_null($max) AND $min > $max) {
+            throw new \Exception(
+                'Validation error: the max length must be least the value of the min length.',
+                E_ERROR
+            );
+        }
+
+        $value = $this->getValue($field);
+        $length = mb_strlen($value);
+        if (
+            !is_null($min) AND
+            !is_null($max) AND
+            ($length < $min OR $length > $max)
+        ) {
+            $this->addError($field, 'between', [$min, $max]);
+            return $this;
+        }
+        if (
+            !is_null($min) AND
+            $length < $min
+        ) {
+            $this->addError($field, 'short', [$min]);
+            return $this;
+        }
+        if (
+            !is_null($max) AND
+            $length > $max
+        ) {
+            $this->addError($field, 'long', [$max]);
+            return $this;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Permet de s'assurer qu'un champ est un slug
+     * @param string $field
+     * @return self
+     */
+    public function slug(string $field): self
+    {
+        $pattern = '/^([a-z0-9]+-?)+$/';
+        $value = $this->getValue($field);
+
+        if (!is_null($value) AND !preg_match($pattern, $value)) {
+            $this->addError($field, 'slug');
+        }
+
+        return $this;
+    }
+
+    public function regex(string $field, string $pattern)
+    {
+        $value = $this->getValue($field);
+        if (!is_null($value) AND !preg_match($pattern, $value)) {
+            $this->addError($field, 'regex'. [$pattern]);
+        }
+    }
+
+    public function dateTime(string $field, string $format = 'Y-m-d H:i:s'): self
+    {
+        $value = $this->getValue($field);
+        $date = DateTime::createFromFormat($format, $value);
+        $errors = DateTime::getLastErrors();
+        if ($errors['error_count'] > 0 OR $errors['warning_count'] > 0 OR $date === false) {
+            $this->addError($field, 'datetime', [$format]);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Permet de récupérer les erreurs.
+     * @return array
+     */
+    public function getErrors(): array
+    {
+        return $this->errors;
+    }
+
+    /**
+     * @return bool Vérifie les données
+     */
+    public function check(): bool
+    {
+        return empty($this->errors);
+    }
+
+    /**
+     * Obtient une valeur en fonction du nom du champ, si le champ n'existe pas, retourne null
+     * @param string $field
+     * @return null|mixed
+     */
+    private function getValue(string $field)
+    {
+        if (array_key_exists($field, $this->fields)) {
+            return $this->fields[$field];
+        }
+        return null;
+    }
+
+    /**
+     * Permet d'instancier et d'ajouter une erreur
+     * @param string $field
+     * @param string $rule
+     */
+    private function addError(string $field, string $rule): void
+    {
+        $this->errors[$field] = new ValidationError($field, $rule);
+    }
+
+}
---
+++ src/Framework/Validator/ValidationError.php
@@ -0,0 +1,34 @@
+<?php
+namespace Framework\Validator;
+
+class ValidationError {
+
+    private $field;
+    private $rule;
+    private $attributes;
+
+    private $messages = [
+        'required'  => 'Le champ %s est requis.',
+        'empty'     => 'Le champ %s ne doit pas être vide.',
+        'between'   => 'Le champs %s doit faire entre %d et %d caractères.',
+        'short'     => 'Le champs %s doit faire au moins %d caractères.',
+        'long'      => 'Le champs %s doit faire moins de %d caractères.',
+        'slug'      => 'Le champ %s n\'est pas une URL valide.',
+        'regex'     => 'Le champ %s ne valide pas l\'expression régulière "%s"',
+        'datetime'  => 'Le champ %s doit correspondre au format %s'
+    ];
+
+    public function __construct(string $field, string $rule, array $attributes = [])
+    {
+        $this->field = $field;
+        $this->rule = $rule;
+        $this->attributes = $attributes;
+    }
+
+    public function __toString(): string
+    {
+        $params = array_merge([$this->messages[$this->rule], $this->field], $this->attributes);
+        return call_user_func_array('sprintf', $params);
+    }
+
+}
