512e18c5ce48e00e7259b64e9303a9aa696373b6
Vasco Compain <vasco.gribouille@gmail.com>
2019-06-15 18:57:58 +0200

    Modification du fonctionnement du FlashService

--- tests/TurboPancake/Services/FlashTest.php
+++ tests/TurboPancake/Services/FlashTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Tests\TurboPancake\Services;
 
+use Traversable;
 use TurboPancake\Services\FlashService;
 use TurboPancake\Services\Session\ArraySession;
 use PHPUnit\Framework\TestCase;
@@ -26,16 +27,21 @@ class FlashTest extends TestCase {
     public function testAddFlash() {
         $this->flash->success('Salut');
 
-        $this->assertEquals('Salut', $this->flash->get('success'));
+        $this->assertIsArray( $this->flash->get('success'));
+        $this->assertIsArray( $this->flash->get('success')[0]);
+        $this->assertEquals('Salut', $this->flash->get('success')[0]['message']);
     }
 
     public function testSuccessfulFlashDeletionAfterDisplay()
     {
         $this->flash->success('Salut');
 
-        $this->assertEquals('Salut', $this->flash->get('success'));
-        $this->assertNull($this->session->get('flash'));
-        $this->assertEquals('Salut', $this->flash->get('success'));
+        $this->assertCount(1, $this->flash->get('success'));
+        $this->assertEquals('Salut', $this->flash->get('success')[0]['message']);
+
+        $this->assertNull($this->session->get('info'));
+        $this->assertCount(1, $this->flash->get('success'));
+        $this->assertEquals('Salut', $this->flash->get('success')[0]['message']);
     }
 
     public function testNoTypeReturnNull()
@@ -46,37 +52,52 @@ class FlashTest extends TestCase {
     public function testInfo()
     {
         $this->flash->info('Salut');
-        $this->assertEquals('Salut', $this->flash->get('info'));
+        $this->assertCount(1, $this->flash->get('info'));
+        $this->assertEquals('Salut', $this->flash->get('info')[0]['message']);
     }
 
     public function testMessage()
     {
         $this->flash->message('Salut');
-        $this->assertEquals('Salut', $this->flash->get('message'));
+        $this->assertCount(1, $this->flash->get('message'));
+        $this->assertEquals('Salut', $this->flash->get('message')[0]['message']);
     }
 
     public function testImportant()
     {
         $this->flash->important('Salut');
-        $this->assertEquals('Salut', $this->flash->get('important'));
+        $this->assertCount(1, $this->flash->get('important'));
+        $this->assertEquals('Salut', $this->flash->get('important')[0]['message']);
     }
 
     public function testSuccess()
     {
         $this->flash->success('Salut');
-        $this->assertEquals('Salut', $this->flash->get('success'));
+        $this->assertCount(1, $this->flash->get('success'));
+        $this->assertEquals('Salut', $this->flash->get('success')[0]['message']);
     }
 
     public function testWarning()
     {
         $this->flash->warning('Salut');
-        $this->assertEquals('Salut', $this->flash->get('warning'));
+        $this->assertCount(1, $this->flash->get('warning'));
+        $this->assertEquals('Salut', $this->flash->get('warning')[0]['message']);
     }
 
     public function testError()
     {
         $this->flash->error('Salut');
-        $this->assertEquals('Salut', $this->flash->get('error'));
+        $this->flash->warning('Test');
+        $this->assertCount(1, $this->flash->get('error'));
+        $this->assertEquals('Salut', $this->flash->get('error')[0]['message']);
+    }
+
+    public function testFullReturn() {
+        $this->flash->error('Salut');
+        $this->flash->warning('Test');
+
+        $this->assertCount(2, $this->flash->get());
+
     }
 
 }
