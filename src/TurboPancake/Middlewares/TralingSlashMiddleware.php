b36311c3035f2e6ce72b4f33ebdb364faa3ed6df
Vasco Compain <vasco.gribouille@gmail.com>
2019-10-27 23:33:22 +0100

    Correction du bug comme quoi le TrailingslashMiddleware bouclait sur la racine.

--- src/TurboPancake/Middlewares/TralingSlashMiddleware.php
+++ src/TurboPancake/Middlewares/TralingSlashMiddleware.php
@@ -12,7 +12,7 @@ class TralingSlashMiddleware implements MiddlewareInterface {
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $uri = $request->getUri()->getPath();
-        if (!empty($uri) AND $uri[-1] === '/') {
+        if ($uri !== '/' AND !empty($uri) AND $uri[-1] === '/') {
             return new Response(301, ['Location' => substr($uri, 0, -1)]);
         }
         return $handler->handle($request);
